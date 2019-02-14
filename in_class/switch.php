3ba12738052b2bcf2621bcae43bbe8b1c6ede212
coding <coding@coding.net>
2019-02-14 16:56:22 +0000

    update by Coding WebIDE , https://ide.coding.net

--- in_class/switch.php
+++ in_class/switch.php
@@ -1,1 +1,23 @@
-switch 3 options and a default
+<?php
+$grade = 10;
+
+switch($grade) {
+    case 10:
+        echo "Perfect";
+        break;
+    case 9:
+        echo "Superb";
+        break;
+    case 8:
+        echo "Excellent";
+        break;
+    case 7:
+        echo "Adequate";
+        break;
+    case 6:
+        echo "Mediocre";
+        break;
+    default:
+        echo "Piss Poor";
+}
+?>
